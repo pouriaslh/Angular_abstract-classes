0e5f54c035e8f5d12b5e2834a7543869f5770ac4
po <pouria_esf2009@yahoo.com>
2021-12-01 08:31:22 +0330

    init

---
+++ 01-helloworld/05-inheritance/Circle.ts
@@ -0,0 +1,16 @@
+import { Shape } from "./Shape";
+
+export class Cirle extends Shape{
+ 
+    
+
+    constructor(theX:number,theY:number,private _radius:number){
+        super(theX,theY);
+    }
+    getInfo():string{
+        return super.getInfo+` , radius=${this._radius}`;
+    }
+    
+
+
+}
